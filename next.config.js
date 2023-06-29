184835a8937e2222dfd698a3204da5ab1c531672
devfront-lucy <136558304+front-lucy@users.noreply.github.com>
2023-06-29 16:43:46 +0900

    fs

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://boramyy.github.io/invitation"
+      : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
